6e296281a68ef823d17b50971dd2f94c8c0346d9
Maxim Meshkov <maksimmeshkovru@gmail.com>
2018-09-03 14:35:11 +0300

    new feature

---
+++ src/main/java/com/company/springproject/App.java
@@ -0,0 +1,34 @@
+package com.company.springproject;
+
+import com.company.springproject.service.TestBeanSpecial;
+import com.company.springproject.service.TestBeanNonSpecial;
+import com.company.springproject.config.ConfigurationPackageTest;
+import com.company.springproject.config.ConfigurationTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args ) {
+
+    }
+
+    private static void newBasedTypeSecondary() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationPackageTest.class);
+        TestBeanSpecial one = context.getBean(TestBeanSpecial.class);
+        TestBeanNonSpecial two = context.getBean(TestBeanNonSpecial.class);
+    }
+
+    private static void newBasedType() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationTest.class);
+        TestBeanSpecial one = context.getBean(TestBeanSpecial.class);
+        TestBeanNonSpecial two = context.getBean(TestBeanNonSpecial.class);
+    }
+
+    private static void oldXmlType() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        TestBeanSpecial one = context.getBean(TestBeanSpecial.class);
+        TestBeanNonSpecial two = context.getBean(TestBeanNonSpecial.class);
+    }
+}
